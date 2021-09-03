ce5db08b4a1e87cd3aecf0a6d8ac6411b41310da
aPailOfCoffee <bshestakoff@gmail.com>
2021-09-03 21:08:17 +0300

    Methods done. Test's ok. Getter added to Resume.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,23 +1,50 @@
 /** Array based storage for Resumes */
 public class ArrayStorage {
-  Resume[] storage = new Resume[10000];
+  private Resume[] storage = new Resume[10000];
+  private int size;
 
-  void clear() {}
+  public void clear() { //done
+    for (int i = 0; i < size; i++) {
+      storage[i] = null;
+    }
+    size = 0;
+  }
 
-  void save(Resume r) {}
+  public void save(Resume r) { //done
+    storage[size++] = r;
+  }
 
-  Resume get(String uuid) {
+  public Resume get(String uuid) {//done
+    for (int i = 0; i < size; i++) {
+      if (storage[i].getUuid().equals(uuid)) {
+        return storage[i];
+      }
+    }
     return null;
   }
 
-  void delete(String uuid) {}
-
-  /** @return array, contains only Resumes in storage (without null) */
-  Resume[] getAll() {
-    return new Resume[0];
+  public void delete(String uuid) {
+    for (int i = 0; i < size; i++) {
+      if (storage[i].getUuid().equals(uuid)) {
+        if (storage[i + 1] == null) {
+          storage[i] = null;
+        } else {
+          System.arraycopy(storage, i + 1, storage, i, size - i - 1);
+        }
+        size--;
+        break;
+      }
+    }
   }
 
-  int size() {
-    return 0;
+  /** @return array, contains only Resumes in storage (without null) */
+  public Resume[] getAll() { //done
+    Resume[] all = new Resume[size];
+    System.arraycopy(storage, 0, all, 0, size);
+    return all;
+  }
+
+  public int size() {//done
+    return size;
   }
 }
--- src/Resume.java
+++ src/Resume.java
@@ -4,6 +4,10 @@ public class Resume {
   // Unique identifier
   String uuid;
 
+  public String getUuid() {
+    return uuid;
+  }
+
   @Override
   public String toString() {
     return uuid;
