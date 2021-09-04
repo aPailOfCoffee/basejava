aa0ad9c4f9dec050a371bf75acaa7c19468531f1
aPailOfCoffee <bshestakoff@gmail.com>
2021-09-04 12:56:40 +0300

    variable all -> resumes. Indent from 2 to 4.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,63 +1,74 @@
-/** Array based storage for Resumes */
+/**
+ * Array based storage for Resumes
+ */
 public class ArrayStorage {
-  private Resume[] storage = new Resume[10000];
-  private int size;
+    private Resume[] storage = new Resume[10000];
+    private int size;
 
-  /** Null all Resumes in the storage */
-  public void clear() {
-    for (int i = 0; i < size; i++) {
-      storage[i] = null;
-    }
-    size = 0;
-  }
-
-  /** @param r - Resume that will be added to the storage */
-  public void save(Resume r) {
-    storage[size++] = r;
-  }
-
-  /**
-   * @param uuid of the searched Resume.
-   * @return Resume with specified uuid
-   */
-  public Resume get(String uuid) {
-    for (int i = 0; i < size; i++) {
-      if (storage[i].getUuid().equals(uuid)) {
-        return storage[i];
-      }
-    }
-    return null;
-  }
-
-  /**
-   * Shifts all Resumes to the left, into the place of the deleted one.
-   * If deleted Resume is last - just null it.
-   * size - i + 1 - count of Resumes that contains after deleted one
-   * @param uuid Resume that should be deleted
-   */
-  public void delete(String uuid) {
-    for (int i = 0; i < size; i++) {
-      if (storage[i].getUuid().equals(uuid)) {
-        if (storage[i + 1] == null) {
-          storage[i] = null;
-        } else {
-          System.arraycopy(storage, i + 1, storage, i, size - i - 1);
+    /**
+     * Null all Resumes in the storage
+     */
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
         }
-        size--;
-        break;
-      }
+        size = 0;
     }
-  }
 
-  /** @return array, contains only Resumes in storage (without null) */
-  public Resume[] getAll() {
-    Resume[] all = new Resume[size];
-    System.arraycopy(storage, 0, all, 0, size);
-    return all;
-  }
+    /**
+     * @param r - Resume that will be added to the storage
+     */
+    public void save(Resume r) {
+        storage[size++] = r;
+    }
 
-  /** @return count of not-null Resumes contained in the storage */
-  public int size() {
-    return size;
-  }
+    /**
+     * @param uuid of the searched Resume.
+     * @return Resume with specified uuid
+     */
+    public Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return storage[i];
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Shifts all Resumes to the left, into the place of the deleted one.
+     * If deleted Resume is last - just null it.
+     * size - i + 1 - count of Resumes that contains after deleted one
+     *
+     * @param uuid Resume that should be deleted
+     */
+    public void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                if (storage[i + 1] == null) {
+                    storage[i] = null;
+                } else {
+                    System.arraycopy(storage, i + 1, storage, i, size - i - 1);
+                }
+                size--;
+                break;
+            }
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        Resume[] resumes = new Resume[size];
+        System.arraycopy(storage, 0, resumes, 0, size);
+        return resumes;
+    }
+
+    /**
+     * @return count of not-null Resumes contained in the storage
+     */
+    public int size() {
+        return size;
+    }
 }
